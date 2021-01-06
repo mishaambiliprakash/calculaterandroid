a5c1f1292ce51c9739def21d14da89d42c9144e2
mishaambiliprakash <ab224105@gmail.com>
2021-01-06 17:40:44 +0530

    created

---
+++ app/src/main/java/com/misha/calculaterappandroid/AdditionActivity.kt
@@ -0,0 +1,29 @@
+package com.misha.calculaterappandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+class AdditionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addition)
+    }
+
+    fun addbutton(view: View) {
+        var getnum1=findViewById<EditText>(R.id.nu1);
+        var getnum2=findViewById<EditText>(R.id.nu2);
+        var nu1=getnum1.text.toString();
+        var nu2=getnum2.text.toString();
+        var result :Double=nu1.toDouble()+nu2.toDouble();
+        Toast.makeText(this,result.toString(),Toast.LENGTH_LONG).show();
+    }
+
+    fun backbutton(view: View) {
+        intent= Intent(this,MainActivity::class.java);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/misha/calculaterappandroid/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.misha.calculaterappandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun addclick(view: View) {
+        intent= Intent(this,AdditionActivity::class.java);
+        startActivity(intent);
+    }
+
+    fun subclick(view: View) {
+        intent= Intent(this,SuntractionActivity::class.java);
+        startActivity(intent);
+    }
+
+    fun mulclick(view: View) {
+        intent= Intent(this,MultiplicationActivity::class.java);
+        startActivity(intent);
+    }
+
+    fun divclick(view: View) {
+        intent= Intent(this,DivisionActivity::class.java);
+        startActivity(intent);
+    }
+}
